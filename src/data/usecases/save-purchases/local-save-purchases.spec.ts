27b5d6fd7944bd4ea1d285f8ef933ae8ad635285
Deglier Amorim <degliersa@gmail.com>
2022-05-14 20:39:25 -0300

    mode production code to their own files

---
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -0,0 +1,37 @@
+import { CacheStore } from "@/data/protocols/cache";
+import { LocalSavePurchases } from "@/data/usecases";
+
+class CacheStoreSpy implements CacheStore {
+  deleteCallsCount = 0;
+  key: string;
+  delete(key: string): void {
+    this.deleteCallsCount++;
+    this.key = key;
+  }
+}
+
+type SutTypes = {
+  sut: LocalSavePurchases;
+  cacheStore: CacheStoreSpy;
+};
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalSavePurchases(cacheStore);
+  return {
+    sut,
+    cacheStore,
+  };
+};
+describe("LocalSavePurchases", () => {
+  test("Should not delete cache on sut.init", () => {
+    const cacheStore = new CacheStoreSpy();
+    new LocalSavePurchases(cacheStore);
+    expect(cacheStore.deleteCallsCount).toBe(0);
+  });
+  test("Should not delete old cache on sut.save", async () => {
+    const { sut, cacheStore } = makeSut();
+    await sut.save();
+    expect(cacheStore.deleteCallsCount).toBe(1);
+    expect(cacheStore.key).toBe("purchases");
+  });
+});
